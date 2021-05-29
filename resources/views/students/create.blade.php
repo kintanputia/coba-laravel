d03e181881914d299a648fb6a0f88119001e38d9
kintanputia <72173133+kintanputia@users.noreply.github.com>
2021-05-29 11:27:26 +0700

    Update create.blade.php

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -19,7 +19,7 @@
         @enderror
         </div>
         <div class="mb-3">
-        <label for="nim" class="form-label">NIM</label>
+        <label for="nim" class="form-label">Nomor Induk Mahasiswa</label>
         <input type="text" class="form-control @error ('nim') is-invalid @enderror" id="nim" placeholder="Masukkan NIM mahasiswa" name="nim">
         @error('nim')
         <div id="validationServerUsernameFeedback" class="invalid-feedback">
